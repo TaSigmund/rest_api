bac7509281deb27008d21b34f1e6bf4c18468fef
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-04-08 13:42:30 +0200

    authentication on the new routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {Course, User} = require('../models');
+const {authenticateUser} = require('../auth-user');
 
 // return a list of all courses including any related user data
 router.get('/', async (req, res, next)=>{
@@ -29,21 +30,21 @@ router.get('/:id', async (req, res, next)=>{
 });
 
 //create a new course
-router.post('/', async (req, res, next)=>{
+router.post('/', authenticateUser, async (req, res, next)=>{
     const createCourse = await Course.create(req.body);
     const lastEntry = await Course.findOne({order: [ [ 'createdAt', 'DESC' ]]});//finds the newest entry in the database
     res.status(201).location(`/courses/${lastEntry.id}`).json(createCourse); //set status, set location header to a path for the newest course, return json
 });
 
 //update the corresponding course
-router.put('/:id', async (req, res, next)=>{
+router.put('/:id', authenticateUser, async (req, res, next)=>{
     const courseToUpdate = await Course.findByPk(req.params.id);
     await courseToUpdate.update(req.body);
     res.status(204).end();
 });
 
 //delete the corresponding course
-router.delete('/:id', async (req, res, next)=>{
+router.delete('/:id', authenticateUser, async (req, res, next)=>{
   const courseToDelete = await Course.findByPk(req.params.id);
   await courseToDelete.destroy();
   res.status(204).end();
